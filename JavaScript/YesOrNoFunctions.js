39a2f4697058b50f1c0d90264e3ca9dd33b70a7a
Bryan Chua <s10223076@connect.np.edu.sg>
2021-12-06 16:32:12 +0800

    Final

---
+++ JavaScript/YesOrNoFunctions.js
@@ -0,0 +1,6 @@
+function yesFunction() {
+    document.getElementById("review").innerHTML = "Hooray! Thank You!";
+}
+function noFunction() {
+    document.getElementById("review").innerHTML = "Aww Man... I'll improve it next time!"
+}
